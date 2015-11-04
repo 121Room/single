e3c3261df4936ae4b2b7766d96694a53e00546a7
JIANG Di <jiang-di@outlook.com>
2015-11-04 22:37:00 +0800

    log some api

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,37 @@
 'use strict'
 
-let t1 = performance.timing.navigationStart
+// The Navigation Timing API
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
+let navEvents = [
+"navigationStart",
+"unloadEventStart",
+"unloadEventEnd",
+"redirectStart",
+"redirectEnd",
+"fetchStart",
+"domainLookupStart",
+"domainLookupEnd",
+"connectStart",
+"connectEnd",
+"secureConnectionStart",
+"requestStart",
+"responseStart",
+"responseEnd",
+"domLoading",
+"domInteractive",
+"domContentLoadedEventStart",
+"domContentLoadedEventEnd",
+"domComplete",
+"loadEventStart",
+"loadEventEnd"]
 
-let t2 = performance.now()
+let time = performance.timing
+let perfData = Object.create(null)
 
-console.log(`t1: ${t1}`)
-console.log(`t2: ${t2}`)
+navEvents.forEach((e)=>{
+  perfData[e] = time[e]
+})
+
+for (let prop in perfData) {
+  console.log(`${prop}: ${perfData[prop]}`)
+}
