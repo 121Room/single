5f19197bfacbc72325d4c0fdd893978f06944f0d
JIANG Di <jiang-di@outlook.com>
2015-11-04 22:38:43 +0800

    fix style

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,27 +3,27 @@
 // The Navigation Timing API
 // https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
 let navEvents = [
-"navigationStart",
-"unloadEventStart",
-"unloadEventEnd",
-"redirectStart",
-"redirectEnd",
-"fetchStart",
-"domainLookupStart",
-"domainLookupEnd",
-"connectStart",
-"connectEnd",
-"secureConnectionStart",
-"requestStart",
-"responseStart",
-"responseEnd",
-"domLoading",
-"domInteractive",
-"domContentLoadedEventStart",
-"domContentLoadedEventEnd",
-"domComplete",
-"loadEventStart",
-"loadEventEnd"]
+  'navigationStart',
+  'unloadEventStart',
+  'unloadEventEnd',
+  'redirectStart',
+  'redirectEnd',
+  'fetchStart',
+  'domainLookupStart',
+  'domainLookupEnd',
+  'connectStart',
+  'connectEnd',
+  'secureConnectionStart',
+  'requestStart',
+  'responseStart',
+  'responseEnd',
+  'domLoading',
+  'domInteractive',
+  'domContentLoadedEventStart',
+  'domContentLoadedEventEnd',
+  'domComplete',
+  'loadEventStart',
+  'loadEventEnd']
 
 let time = performance.timing
 let perfData = Object.create(null)
