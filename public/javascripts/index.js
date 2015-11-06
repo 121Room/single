e0cf31d204afcc90a179d67be6a5b0abbfd822e5
JIANG Di <jiang-di@outlook.com>
2015-11-06 19:14:20 +0800

    rewrite all

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,44 +1,11 @@
 'use strict'
 
 let hello = require('./hello')
-
-// TODO run in phantomjs
-
-// The Navigation Timing API
-// https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API
-let navEvents = [
-  'navigationStart',
-  'unloadEventStart',
-  'unloadEventEnd',
-  'redirectStart',
-  'redirectEnd',
-  'fetchStart',
-  'domainLookupStart',
-  'domainLookupEnd',
-  'connectStart',
-  'connectEnd',
-  'secureConnectionStart',
-  'requestStart',
-  'responseStart',
-  'responseEnd',
-  'domLoading',
-  'domInteractive',
-  'domContentLoadedEventStart',
-  'domContentLoadedEventEnd',
-  'domComplete',
-  'loadEventStart',
-  'loadEventEnd']
-
-let time = performance.timing
-let perfData = Object.create(null)
-
-navEvents.forEach((e)=>{
-  perfData[e] = time[e]
-})
-
-for (let prop in perfData) {
-  //TODO post to backend, save to database and show it
-  console.log(`${prop}: ${perfData[prop]}`)
-}
+let timing = require('timing_commonjs')
 
 hello()
+
+window.onload = () => {
+  window.timing = timing()
+  console.log(window.timing)
+}
