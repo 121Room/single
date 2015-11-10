7e6e5af270b829e498c4111025defe10dc0075fa
JIANG Di <jiang-di@outlook.com>
2015-11-10 18:29:16 +0800

    log result

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,11 @@
 'use strict'
 
 let fib = require('fibonacci-perf')
+let timing = require('timing_commonjs')
+
 
 document.addEventListener('DOMContentLoaded', () => console.log(fib(41)))
 
-window.onload = () => console.log(fib(41))
+window.onload = () => {
+  console.log(timing()) // TODO post to backend
+}
