9b1239ce1dbd009b4f81bba0c16ebacf48e2329a
JIANG Di <jiang-di@outlook.com>
2015-11-10 19:07:52 +0800

    fix :bug:

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => console.log(fib(41)))
 window.onload = () => {
   let request = new XMLHttpRequest()
   request.open('POST', 'http://localhost:4567/report', true)
-  request.setRequestHeader('Content-Type', 'application/json charset=UTF-8')
+  request.setRequestHeader('Content-Type', 'application/json')
   let data = timing()
   request.send(JSON.stringify(data))
   console.log(data)
