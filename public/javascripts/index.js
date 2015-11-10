e6db9be80ec153aa3c982c9935aa0f8f40d5d6fb
JIANG Di <jiang-di@outlook.com>
2015-11-10 23:06:58 +0800

    for debug purpose

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,7 +4,7 @@ let fib = require('fibonacci-perf')
 let timing = require('timing_commonjs')
 
 
-document.addEventListener('DOMContentLoaded', () => console.log(fib(41)))
+console.log(fib(41))
 
 window.onload = () => {
   let request = new XMLHttpRequest()
