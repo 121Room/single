4ce8851165d5b0debaa40d2b0dd1e3001198f0e6
JIANG Di <jiang-di@outlook.com>
2015-11-12 23:02:19 +0800

    :art:

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -3,13 +3,11 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceNavigation
 
 module.exports = () => {
-  let api = Object.create(null)
   const nav = performance.navigation
-  for (let prop in nav) {
-    if ((prop === 'type') || (prop === 'redirectCount')) {
-      api[prop] = nav[prop]
-    }
-  }
 
+  let api = Object.create(null)
+  api.type = nav.type
+  api.redirectCount = nav.redirectCount
+  
   return api
 }
