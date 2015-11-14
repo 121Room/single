3322861d8d4fcbc5ecc74b2a57d0884503333fe2
JIANG Di <jiang-di@outlook.com>
2015-11-14 22:24:58 +0800

    add lastImgTiming to index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,6 +5,7 @@ const post = require('./post')
 const getPosition = require('./position')
 const getNav = require('./nav')
 const getNetwork = require('./network')
+const getLastImgTiming = require('./resource_timing').lastImgTiming
 
 window.onload = () => {
   const ua = navigator.userAgent
@@ -14,8 +15,11 @@ window.onload = () => {
   const lang = navigator.language
   const resolution = {width: screen.width, height: screen.height}
   const network = getNetwork()
+  const resourceTiming = performance.getEntriesByType('resource')
+  const lastImgTiming = getLastImgTiming(resourceTiming)
 
-  let data = { ua, timing, url, nav, lang, resolution, network }
+  console.log(lastImgTiming)
+  let data = { ua, timing, url, nav, lang, resolution, network, lastImgTiming }
 
   navigator.geolocation.getCurrentPosition((postion) => {
     const ps = getPosition(postion)
