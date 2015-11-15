fc0311c3e549ed09f48fd74a6527065a6f1956d1
lv dada <417611525@qq.com>
2015-11-16 00:06:07 +0800

    首屏doing not done

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let gulp = require('gulp')
 let eslint = require('gulp-eslint')
 
 gulp.task('lint', () => {
-  return gulp.src(['**/*.js', '!node_modules/**', '!dist/**', '!webpack.config.js'])
+  return gulp.src(['!public/**', '**/*.js', '!node_modules/**', '!dist/**', '!webpack.config.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
--- javascripts/resource_timing/last_img_timing.js
+++ javascripts/resource_timing/last_img_timing.js
@@ -20,3 +20,74 @@ module.exports = (resourceTiming) => {
 
   return getResourceTimingData(lastImgTiming)
 }
+
+//首屏时间尝试：
+  //1,获取首屏基线高度
+  //2,计算出基线dom元素之上的所有图片元素
+  //3,所有图片onload之后为首屏显示时间
+  //
+  function getOffsetTop(ele) {
+    var offsetTop = ele.offsetTop;
+    if (ele.offsetParent !== null) {
+      offsetTop += getOffsetTop(ele.offsetParent);
+    }
+    return offsetTop;
+  }
+
+  var firstScreenHeight = window.screen.height;
+  var firstScreenImgs = [];
+  var isFindLastImg = false;
+  var allImgLoaded = false;
+  var imgs = [];
+  var img;
+  
+  imgs = document.body.querySelector('img');
+  for (var i = 0; i < imgs.length; i++) {
+    img = imgs[i];
+    var imgOffsetTop = getOffsetTop(img);
+    if (imgOffsetTop > firstScreenHeight) {
+      continue;
+    } else if (imgOffsetTop <= firstScreenHeight && !img.hasPushed) {
+      img.hasPushed = 1;
+      firstScreenImgs.push(img);
+    }
+  }
+  
+  // var t = setInterval(function() {
+  //   var i, img;
+  //   if (isFindLastImg) {
+  //     if (firstScreenImgs.length) {
+  //       for (i = 0; i < firstScreenImgs.length; i++) {
+  //         img = firstScreenImgs[i];
+  //         if (!img.complete) {
+  //           allImgLoaded = false;
+  //           break;
+  //         } else {
+  //           allImgLoaded = true;
+  //         }
+  //       }
+  //     } else {
+  //       allImgLoaded = true;
+  //     }
+  //     if (allImgLoaded) {
+  //       collect.add({
+  //         firstScreenLoaded: startTime - Date.now()
+  //       });
+  //       clearInterval(t);
+  //     }
+  //   } else {  //没有找到超出屏幕的图片
+  //     var imgs = document.body.querySelector('img');
+  //     for (i = 0; i < imgs.length; i++) {
+  //       img = imgs[i];
+  //       var imgOffsetTop = getOffsetTop(img);
+  //       if (imgOffsetTop > firstScreenHeight) {
+  //         isFindLastImg = true;
+  //         break;
+  //       } else if (imgOffsetTop <= firstScreenHeight && !img.hasPushed) {
+  //         img.hasPushed = 1;
+  //         firstScreenImgs.push(img);
+  //       }
+  //     }
+  //   }
+  // }, 10);
+  
